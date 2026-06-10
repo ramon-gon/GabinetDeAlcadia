8e6e1856a3de2bee339a3bbcbbe1697c8eecc2fe
Ramon <ramonpersonal10@gmail.com>
2026-06-10 12:18:55 +0200

    add email permissions and update view/menu policies in CRUDAccessRole

--- src/main/java/com/company/gabinetdealcadia/security/CRUDAccessRole.java
+++ src/main/java/com/company/gabinetdealcadia/security/CRUDAccessRole.java
@@ -13,23 +13,33 @@ import io.jmix.securityflowui.role.annotation.ViewPolicy;
 public interface CRUDAccessRole {
     String CODE = "c-r-u-d-access-role";
 
-    // 1. Permisos d'Entitat
+    // 1. Permisos d'Entitat (Accions sobre les files)
     @EntityPolicy(entityClass = Carrec.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = Contacte.class, actions = EntityPolicyAction.ALL)
     @EntityPolicy(entityClass = Entitat.class, actions = EntityPolicyAction.ALL)
     void entityPermissions();
 
-    // 2. Permisos d'Atributs
+    // 2. Permisos d'Atributs (Accions sobre les columnes - CRUCIAL PER EVITAR EL MISSATGE D'ERROR)
+    // Utilitzem VIEW per a lectura i MODIFY per a edició
     @EntityAttributePolicy(entityClass = Carrec.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     @EntityAttributePolicy(entityClass = Contacte.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     @EntityAttributePolicy(entityClass = Entitat.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
     void attributePermissions();
 
-    // 3. Permisos de Vistes (SENSE asterisc)
-    @ViewPolicy(viewIds = {"Carrec.list", "Carrec.detail", "Contacte.list", "Contacte.detail", "Entitat.list", "Entitat.detail"})
+    // 3. Permisos de Vistes
+    @ViewPolicy(viewIds = {
+            "Carrec.list", "Carrec.detail",
+            "Contacte.list", "Contacte.detail",
+            "Entitat.list", "Entitat.detail",
+            "EnviarCorreu"
+    })
     void viewPermissions();
 
-    // 4. Permisos de Menú (SENSE asterisc)
-    @MenuPolicy(menuIds = {"Carrec.list", "Contacte.list", "Entitat.list"})
+    // 4. Permisos de Menú
+    @MenuPolicy(menuIds = {"Carrec.list", "Contacte.list", "Entitat.list", "EnviarCorreu"})
     void menuPermissions();
+
+    // 5. Permís específic per a l'enviament de correus
+    @SpecificPolicy(resources = "io.jmix.email.send")
+    void emailPermissions();
 }
