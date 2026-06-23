2c1d5347b97670263dde0c6e147534a77a993a50
Ramon <ramonpersonal10@gmail.com>
2026-06-23 17:28:03 +0200

    add AppShell class to configure visual styles for Vaadin 24 / Jmix 2.x

---
+++ src/main/java/com/company/gabinetdealcadia/AppShell.java
@@ -0,0 +1,11 @@
+package com.company.gabinetdealcadia;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+/**
+ * 🌟 Esta es la clase que exige Vaadin 24 / Jmix 2.x para gestionar los estilos visuales del proyecto.
+ */
+@Theme(value = "gabinetdealcadia")
+public class AppShell implements AppShellConfigurator {
+}
